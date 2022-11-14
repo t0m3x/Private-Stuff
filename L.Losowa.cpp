e8b07324558446a3c21169ffb9f3bcbe1828a3c5
t0m3x <60542862+t0m3x@users.noreply.github.com>
2022-11-14 09:27:02 +0100

    Add files via upload

---
+++ L.Losowa.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int tablica[20];
+int n = sizeof(tablica) / sizeof(tablica[0]);
+
+
+void bubbleSort(int tablica[], int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (tablica[j] > tablica[j + 1]) {
+                swap(tablica[j], tablica[j + 1]);
+            }
+        }
+    }
+
+       cout <<
+}
+
+int main()
+{
+    srand(time(NULL));
+
+    for (int i = 0; i <= 19; i++) {
+        tablica[i] = rand() % 101;
+    }
+    
+    bubbleSort(tablica, n);
+
+    cout << tablica[19] << endl;
+
+    for (int j = 0; j <= 19; j++) {
+        cout << tablica[j] << endl;
+    }
+
+    return 0;
+}
