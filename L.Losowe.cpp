8c545ecc03f7109b6374c2d8d8dc39814b16da5e
t0m3x <60542862+t0m3x@users.noreply.github.com>
2022-11-20 22:43:28 +0100

    Add files via upload

---
+++ L.Losowe.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <time.h>
+#include <cstring>
+#include <ctype.h>
+
+using namespace std;
+
+int ml(string slowo) {
+    int ilosc = 0;
+    string litery;
+    int pos = 0;
+
+    /*for (int j = 0; j < slowo.size(); j++) {
+        if (isalpha(slowo[j])) {
+            litery[pos] = slowo[j];
+            pos++;
+        }
+    }*/
+    for (int i = 0; i < slowo.size(); i++) {
+        if (!isupper(slowo[i])) {
+            ilosc++;
+        }
+    }
+
+    return ilosc;
+}
+
+int lacina(string slowo) {
+
+    int ilosc = 0;
+    for (int i = 0; i < slowo.size(); i++){
+        if (!isalpha(slowo[i])) {
+            ilosc++;
+        }
+    }
+
+    return ilosc;
+}
+
+int main()
+{
+    string slowo;
+
+    cout << "Podaj jakies slowo:";
+    cin >> slowo;
+
+    cout << "\nSlowo zawiera " << ml(slowo) << " malych liter\n";
+    cout << "Slowo zawiera " << lacina(slowo) << " liter spoza laciny";
+
+    return 0;
+}
